e453a8fc9b34469077b18b5b2a8073332bfff03f
aussiearef <arefkr@gmail.com>
2018-02-13 04:42:53 +0000

    Added phrasechec.ps1

---
+++ phrasecheck.ps1
@@ -0,0 +1,24 @@
+﻿Param
+(
+    [string] $fileName,
+    [string] $phrase,
+    [int] $threshold=1
+)
+
+$allText = Get-Content -Path $fileName
+$matches=  Select-String -InputObject $allText -Pattern $phrase -AllMatches
+$matchCount = $matches.Matches.Count
+
+Write-Output $matches.Matches.Count
+
+
+if ($matchCount -gt $threshold)
+{
+    $message = "$filename includes $matchCount occurances of $phrase. Threshold i s $threshold. "
+    Write-Output $message
+    throw $message
+}
+else
+{
+    Write-Output "$filename includes no more than $threshold occurances of $phrase. OK!"
+}
